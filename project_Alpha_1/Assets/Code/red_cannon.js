ba3d587121ea8e74b29bafdc03edf9e3cea6c6d4
Jiggedy <therealao@gmail.com>
2015-11-02 10:52:59 +0000

    Update

---
+++ project_Alpha_1/Assets/Code/red_cannon.js
@@ -0,0 +1,35 @@
+#pragma strict
+
+
+
+var object_pos:Vector3;
+var angle:float;
+var rotationVector:Vector3;
+var turnSpeed:int = 2;
+
+	// Use this for initialization
+	function Start () {
+	}
+
+	function Update(){ 
+	}
+
+	function FixedUpdate () {    
+		//
+		
+		object_pos = Camera.main.WorldToScreenPoint(transform.position);
+		
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+		angle -= turnSpeed;
+		rotationVector = new Vector3 (0, 0, angle);
+		transform.rotation = Quaternion.Euler(rotationVector);
+		}
+		else if (Input.GetKey(KeyCode.RightArrow))
+		{
+		angle += turnSpeed;
+		rotationVector = new Vector3 (0, 0, angle);
+		transform.rotation = Quaternion.Euler(rotationVector);
+		}
+		
+	}
