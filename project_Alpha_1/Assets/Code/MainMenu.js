565607adf3671853dfba39d23cbd0b3f26991257
DHanley218003 <bassmaster965@yahoo.com>
2015-11-02 23:15:52 +0000

    Merging to main

---
+++ project_Alpha_1/Assets/Code/MainMenu.js
@@ -0,0 +1,44 @@
+﻿#pragma strict
+var CurrentMenu:String;
+function Start()
+{
+	CurrentMenu = "Main";
+}
+
+function OnGUI()
+{
+	if(CurrentMenu == "Main")
+		MainMenu();
+	if(CurrentMenu == "Credits")
+		Credits();
+}
+
+function NavGate(nextMenu:String)
+{
+	CurrentMenu = nextMenu;
+}
+
+function MainMenu()
+{
+	if(GUI.Button (new Rect(500, 150, 200, 50), "Start"))
+	{
+		Application.LoadLevel("AoW");
+	}
+	if(GUI.Button (new Rect(500, 200, 200, 50), "Credits"))
+	{
+		NavGate("Credits");
+	}
+	if(GUI.Button (new Rect(500, 250, 200, 50), "Exit"))
+	{
+		Application.Quit();
+	}
+}
+
+function Credits()
+{
+	(GUI.Button (new Rect(500, 150, 200, 100), "Developed by James Blakeley,\n Adrian Flaherty,\nArthur Franca\n and Declan Hanley"));
+	if(GUI.Button (new Rect(500, 250, 200, 50), "Back"))
+	{
+		NavGate("Main");
+	}
+}
